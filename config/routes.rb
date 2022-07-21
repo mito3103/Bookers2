25207df7821589ac156c9804d0a275c5b849333b
mito3103 <105275723+mito3103@users.noreply.github.com>
2022-07-21 15:45:54 +0000

    順調

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+
   resources :users, only: [:show, :edit]
 
   resources :books, only: [:new, :index, :show, :destroy]
 
   get 'homes/top'
-  devise_for :users
+  get 'homes/about'
+
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
