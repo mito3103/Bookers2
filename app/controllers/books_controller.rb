9608cad7f93bc845e080b6fd9ebe4459081dca83
mito3103 <105275723+mito3103@users.noreply.github.com>
2022-08-09 11:51:32 +0000

    バリデーションおｋ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,11 +21,19 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
+    @profile_image = current_user.profile_image
+    @name = current_user.name
+    @intro = current_user.introduction
+    @user = current_user
+    @books = Book.all
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(book)
+    else
+      render :index
+    end
   end
 
   def edit
